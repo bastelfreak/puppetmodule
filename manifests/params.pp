58957a248f3a6cb8edec343cec88a0a54165bdc8
Paolo Cadiz <pcadiz@cambridge.org>
2016-02-03 14:42:14 +0800

    added ruby_dev params

--- manifests/params.pp
+++ manifests/params.pp
@@ -53,6 +53,7 @@ class puppet::params {
       $puppet_ssldir                = '/var/lib/puppet/ssl'
       $passenger_package            = 'mod_passenger'
       $rack_package                 = 'rubygem-rack'
+      $ruby_dev                     = 'ruby-devel'
     }
     'Suse': {
       $puppet_master_package        = 'puppet-server'
@@ -76,6 +77,7 @@ class puppet::params {
       $puppet_ssldir                = '/var/lib/puppet/ssl'
       $passenger_package            = 'libapache2-mod-passenger'
       $rack_package                 = 'librack-ruby'
+      $ruby_dev                     = 'ruby-dev'
     }
     'FreeBSD': {
       $puppet_agent_service         = 'puppet'
